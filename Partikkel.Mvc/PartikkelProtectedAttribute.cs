24cf79334347f46cbd525d973d187697d6a697f1
Jarle Tvedten <jarle@miles.no>
2017-02-28 23:24:27 +0100

    Added action filter for ASP.NET MVC solutions

---
+++ Partikkel.Mvc/PartikkelProtectedAttribute.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Partikkel.Mvc
+{
+    public class PartikkelProtectedAttribute : ActionFilterAttribute
+    {
+        public string CertPath { get; set; }
+        public string PaywallUrl { get; set; }
+
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            var url = filterContext.RequestContext.HttpContext.Request.Url;
+            var path = String.Format("{0}{1}{2}{3}", url.Scheme, Uri.SchemeDelimiter, url.Authority, url.AbsolutePath);
+            var token = filterContext.RequestContext.HttpContext.Request.QueryString["partikkel"];
+            var urlList = HttpContext.Current.Session["Particket"] as List<string> ?? new List<string>();
+
+            if (!urlList.Contains(path))
+            {
+                ValidateToken(token, path, urlList);
+            }
+            base.OnActionExecuting(filterContext);
+        }
+
+        private void ValidateToken(string token, string path, List<string> urlList)
+        {
+            if (string.IsNullOrEmpty(token))
+            {
+                HttpContext.Current.Response.Redirect(PaywallUrl);
+            }
+            else
+            {
+                //Validate token
+                var validator = new PartikkelValidator();
+                var result = validator.Validate(token, CertPath);
+                if (result == null)
+                {
+                    HttpContext.Current.Response.Redirect(PaywallUrl);
+                    return;
+                }
+
+                //Check that the token url matches the current url    
+                var purchasedUrl = result["url"] as string;
+                //string path = String.Format("{0}{1}{2}{3}", url.Scheme, Uri.SchemeDelimiter, url.Authority, url.AbsolutePath);
+                if (purchasedUrl == null || !purchasedUrl.Contains(path))
+                {
+                    HttpContext.Current.Response.Redirect(PaywallUrl);
+                }
+                else
+                {
+                    urlList.Add(path);
+                }
+            }
+        }
+    }
+}
---
+++ Partikkel.Mvc/PartikkelValidator.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+
+namespace Partikkel.Mvc
+{
+    public class PartikkelValidator
+    {
+        public IDictionary<string, object> Validate(string token, string certPath)
+        {
+            var bytes = Convert.FromBase64String(token);
+            var result = Encoding.UTF8.GetString(bytes);
+            var pubKey = LoadCert(certPath);
+            var claims = Jose.JWT.Decode<IDictionary<string, object>>(result, pubKey);
+            ValidateJwtToken(claims);
+            return claims;
+        }
+
+        private void ValidateJwtToken(IDictionary<string, object> claims)
+        {
+            var expiry = claims["exp"];
+            var unixEpoc = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
+            var expiryDouble = Convert.ToDouble((Int32)expiry);
+            var expiryDate = unixEpoc.AddSeconds(expiryDouble);
+            var currentDate = DateTime.UtcNow;
+            if (currentDate > expiryDate)
+            {
+                throw new Exception("JSON token has expired.");
+            }
+        }
+
+        private RSACryptoServiceProvider LoadCert(string path)
+        {
+            return (RSACryptoServiceProvider)X509(path).PublicKey.Key;
+        }
+
+        private X509Certificate2 X509(string path)
+        {
+            return new X509Certificate2(path, "", X509KeyStorageFlags.Exportable | X509KeyStorageFlags.MachineKeySet);
+        }
+    }
+}
